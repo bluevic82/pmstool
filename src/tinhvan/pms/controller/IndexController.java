68c2ff3d837594842c29667818c51ee2ba762d87
bluevic82 <thangnn1@gmail.com>
2017-11-24 11:31:18 +0700

    add project structure

---
+++ src/tinhvan/pms/controller/IndexController.java
@@ -0,0 +1,13 @@
+package tinhvan.pms.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+	@RequestMapping
+	public String view() {
+		return "index";
+	}
+}
