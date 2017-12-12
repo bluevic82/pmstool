30ce614cf5e6283f8bc3d8cb6aaa5dfc89876a30
luong <luongheroes@gitbub.com>
2017-12-12 13:50:24 +0700

    commit model project

---
+++ src/main/java/com/tinhvan/model/ProjectInfo.java
@@ -0,0 +1,66 @@
+package com.tinhvan.model;
+
+
+public class ProjectInfo {
+	private String project_name;
+	private String project_from;
+	private String project_to;
+	private int charge_cost;
+	private int status;
+	private int type;
+	private String description;
+	private String technical;
+	public String getProject_name() {
+		return project_name;
+	}
+	public void setProject_name(String project_name) {
+		this.project_name = project_name;
+	}
+	public String getProject_from() {
+		return project_from;
+	}
+	public void setProject_from(String project_from) {
+		this.project_from = project_from;
+	}
+	public String getProject_to() {
+		return project_to;
+	}
+	public void setProject_to(String project_to) {
+		this.project_to = project_to;
+	}
+	public int getCharge_cost() {
+		return charge_cost;
+	}
+	public void setCharge_cost(int charge_cost) {
+		this.charge_cost = charge_cost;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getTechnical() {
+		return technical;
+	}
+	public void setTechnical(String technical) {
+		this.technical = technical;
+	}
+	public ProjectInfo() {
+		super();
+	}
+	
+	
+}
---
+++ src/main/java/com/tinhvan/model/ScopeProject.java
@@ -0,0 +1,26 @@
+package com.tinhvan.model;
+
+public class ScopeProject {
+	private int project_id;
+	private int scope_id;
+	private int scope_project_id;
+	public int getProject_id() {
+		return project_id;
+	}
+	public void setProject_id(int project_id) {
+		this.project_id = project_id;
+	}
+	public int getScope_id() {
+		return scope_id;
+	}
+	public void setScope_id(int scope_id) {
+		this.scope_id = scope_id;
+	}
+	public int getScope_project_id() {
+		return scope_project_id;
+	}
+	public void setScope_project_id(int scope_project_id) {
+		this.scope_project_id = scope_project_id;
+	}
+	
+}
